02fb2a652119efc7875081cd8aafc3cd5c791402
Vishwavish30 <vishwapremkumar30@gmail.com>
2026-06-04 14:53:21 +0530

    updated the API for hosting

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ app.get("/", (req, res) => {
   res.send("Backend Running Successfully")
 })
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
